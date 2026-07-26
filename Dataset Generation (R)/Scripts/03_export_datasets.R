803c20b92133dcf3f1538720086941ece7dff215
Shreyas Yadav <shreyasyadav102@gmail.com>
2026-07-26 20:52:17 +1000

    Create dataset export script
    Added script to export clean and dirty datasets to Excel files.

---
+++ Dataset Generation (R)/Scripts/03_export_datasets.R
@@ -0,0 +1,14 @@
+library(openxlsx)
+source("02_generate_dirty_data.R")
+
+# ---------------- Create dataset lists ------------
+
+clean_datasets <- list(Brighton = brighton_clean, Carlton = carlton_clean, `South Wharf` = south_wharf_clean, Dandenong = dandenong_clean)
+dirty_datasets <- list(Brighton = brighton_dirty, Carlton = carlton_dirty, `South Wharf` = south_wharf_dirty, Dandenong = dandenong_dirty)
+
+# ---------------- Export datasets ----------------
+
+dir.create("../Generated Datasets", showWarnings = FALSE)
+
+export_workbook(clean_datasets, "../Generated Datasets/newsvendor_clean_4stores.xlsx")
+export_workbook(dirty_datasets, "../Generated Datasets/newsvendor_dirty_4stores.xlsx")
